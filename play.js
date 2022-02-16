ecdc41c90d2e2111677a24deda9881ba0d2d38ba
Gabriel Celligoi <gabrielcelligoi@gmail.com>
2022-02-16 21:12:52 +0000

    fix the bug in the ctrl^c

--- play.js
+++ play.js
@@ -1,20 +1,9 @@
 const net = require("net");
 const connect = require("./client");
-const handleUserInput = function () {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+const setupInput = require("./input");
+
 
 console.log("Connecting ...");
 connect();
 
-// setup interface to handle user input from stdin
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+setupInput();
