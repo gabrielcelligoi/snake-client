c32af1a4fe795331e791efcf77d1a0a17ca14b6a
Gabriel Celligoi <gabrielcelligoi@gmail.com>
2022-02-16 21:46:48 +0000

    implement key movements and send this to the server

--- input.js
+++ input.js
@@ -1,12 +1,28 @@
+// Stores the active TCP connection object.
+let connection;
+
 //function to handle the imput from the user keybord
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === 'w') {
+    connection.write('Move: up');
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
 };
 
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
