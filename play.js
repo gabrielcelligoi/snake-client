2ef8da8d338a6679cfc6fc2a12429f5cd1b19091
Gabriel Celligoi <gabrielcelligoi@gmail.com>
2022-02-16 21:02:19 +0000

    implementing stdin and handleUserInput in client.js

--- play.js
+++ play.js
@@ -1,8 +1,20 @@
 const net = require("net");
 const connect = require("./client");
-
+const handleUserInput = function () {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
 
 console.log("Connecting ...");
 connect();
 
-// module.exports = {connect}
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
